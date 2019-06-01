cf46db9f57541d9b905e74f57880ddbb4f10f071
Lucas Ricciardi de Salles <lucas.salles@4linux.com.br>
2019-06-01 17:08:22 -0300

    aula  3

--- blueprints/jenkins.py
+++ blueprints/jenkins.py
@@ -1,5 +1,12 @@
 
 import flask
+import jenkins
+
+URL = 'http://localhost:8080'
+USER = '4linux'
+PASSWORD = '4linux123'
+
+connection = jenkins.Jenkins(URL, USER, PASSWORD)
 
 blueprint = flask.Blueprint('jenkins', __name__)
 
@@ -11,13 +18,13 @@ def get_jenkins():
         'route': {
             'is_public': False
         },
+        'jobs': [ 
+            connection.get_job_info(j['name']) 
+                for j in connection.get_jobs()
+        ]
     }
 
     return flask.render_template('jenkins.html', context=context)
-
-@blueprint.route('/jenkins', methods=[ 'POST' ])
-def post_jenkins():
-    pass
 
 @blueprint.route('/jenkins/update/<string:jobname>', methods=[ 'GET' ])
 def get_jenkins_update(jobname):
@@ -27,10 +34,17 @@ def get_jenkins_update(jobname):
         'route': {
             'is_public': False
         },
+        'job': {
+            'name': jobname,
+            'xml': connection.get_job_config(jobname)
+        }
     }
 
     return flask.render_template('jenkins_update.html', context=context)
 
-@blueprint.route('/jenkins', methods=[ 'POST' ])
-def post_jenkins_update():
-    pass
+@blueprint.route('/jenkins/build/<string:jobname>', methods=[ 'GET' ])
+def get_jenkins_build(jobname):
+
+    connection.build_job(jobname)
+
+    return flask.redirect('/jenkins')
