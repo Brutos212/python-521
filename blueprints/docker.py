451704b3830480dc7eb369daf6934bdbe29e39e2
Lucas Ricciardi de Salles <lucas.salles@4linux.com.br>
2019-06-01 14:16:19 -0300

    docker dashboard

--- blueprints/docker.py
+++ blueprints/docker.py
@@ -19,12 +19,20 @@ def get_docker():
 
     return flask.render_template('docker.html', context=context)
 
-@blueprint.route('/docker/start', methods=[ 'GET' ])
-def start_docker():
-
+@blueprint.route('/docker/start/<string:containerid>', methods=[ 'GET' ])
+def start_docker(containerid):
+    try:
+        container = connection.containers.get(containerid)
+        container.start()
+    except:
+        pass
     return flask.redirect('/docker')
 
-@blueprint.route('/docker/stop', methods=[ 'GET' ])
-def stop_docker():
-
+@blueprint.route('/docker/stop/<string:containerid>', methods=[ 'GET' ])
+def stop_docker(containerid):
+    try:
+        container = connection.containers.get(containerid)
+        container.stop()
+    except:
+        pass
     return flask.redirect('/docker')
